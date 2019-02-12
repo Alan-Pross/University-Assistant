91f7cd1580af00ecc49a54248a29bb92f60d1a3d
Pro.§ <874798417@qq.com>
2019-02-13 00:02:33 +0800

    no commit message

--- xcx/pages/index/index.js
+++ xcx/pages/index/index.js
@@ -6,7 +6,6 @@ Page({
   data: {
     percent: 0,
     status: 'normal',
-    motto: '欢迎进入江大小助手',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
@@ -26,9 +25,7 @@ Page({
         hasUserInfo: true
       },
       this.goProgress())
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
+    } else {
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
@@ -36,18 +33,6 @@ Page({
         },
         this.goProgress())
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          },
-          this.goProgress())
-        }
-      })
     }
   },
   getUserInfo: function(e) {
